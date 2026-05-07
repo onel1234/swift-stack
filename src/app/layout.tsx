3f85ff2f5289a20d445b8066376baa481fff22a2
onel1234 <wathilaonelranaweera9@gmail.com>
2026-05-07 10:11:09 +0530

    tab icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Visionary Web Agency',
+  title: 'Swift Stack | Web Agency',
   description: 'Architecting the Future of Web Experiences',
 };
 
